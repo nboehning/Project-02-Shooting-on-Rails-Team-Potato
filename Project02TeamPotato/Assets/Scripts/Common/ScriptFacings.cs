a54c7def18e38ff7d3bca8127b9f3e9cbfbc16a5
nboehning <nboehning@gmail.com>
2015-09-09 18:51:58 -0500

    Added a waypoint super class and started facings script
    Make each individual class a subclass of the waypoint class, like I did
    in the facings script. Facing script has the free look code, but not in
    the correct format for implementation. waypoint class is currently blank
    but will be the code that runs everything. It'll call the
    functions/states from the other classes as needed.

---
+++ Project02TeamPotato/Assets/Scripts/Common/ScriptFacings.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Facings : Waypoint
+{
+    float xRotation;
+    float yRotation;
+    float lookSensitivity = 5f;
+    float curXRotation;
+    float curYRotation;
+    float lookSmoothDamp = 0.1f;
+    float xRotationV = 0;
+    float yRotationV = 0;
+
+    GameObject mainCamera = GameObject.Find("Main Camera");
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        curXRotation = Mathf.SmoothDamp(curXRotation, xRotation, ref xRotationV, lookSmoothDamp);
+        curYRotation = Mathf.SmoothDamp(curYRotation, yRotation, ref yRotationV, lookSmoothDamp);
+
+        mainCamera.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+	}
+}
