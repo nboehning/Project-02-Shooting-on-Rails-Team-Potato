3ab5084efd3df1191a25a0c3a9b728cc8cb869b8
nboehning <nboehning@gmail.com>
2015-09-10 23:58:35 -0500

    Began work on inspector
    Renamed and redid several classes to implement a custom inspector.

---
+++ Project02TeamPotato/Assets/Scripts/Common/MovementTypes.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MovementType
+{
+    STRAIGHTLINE,
+    BEZIERCURVE,
+    LOOKANDRETURN,
+    WAIT
+}
+
+public enum CameraEffectTypes
+{
+    SPLATTER,
+    SHAKE,
+    FADE,
+    NONE
+}
+
+public enum FacingTypes
+{
+    FREELOOK,
+    FIXEDPOINT
+}
+
+[System.Serializable]
+public class MovementTypes
+{
+    public MovementType moveType;
+    public float waypointDuration;
+
+    public Transform point1;
+    public Transform point2;
+    public Transform point3;
+    public Transform lookPoint;
+
+    public float lookTime1;
+    public float lookTime2;
+    public float lookTime3;
+
+    public FacingTypes facingType;
+    public CameraEffectTypes effectType;
+
+    public float splatterFadeSpeed;
+    public bool splatterFade;
+    public float shakeIntensity;
+}
